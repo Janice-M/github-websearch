32c76ad258484ef81fb00eb647bd57fade5a7a0e
Janice-M <janiceink001@gmail.com>
2019-07-14 16:38:34 +0300

    class plus component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/github.spec.ts
@@ -0,0 +1,7 @@
+import { Github } from './github';
+
+describe('Github', () => {
+  it('should create an instance', () => {
+    expect(new Github()).toBeTruthy();
+  });
+});
