d273fdb5869a66602f448369c2544a96f5d44567
Janice-M <janiceink001@gmail.com>
2019-07-15 06:39:21 +0300

    user class added

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
-}
+    constructor(
+      public avatar: any,
+      public username:string,
+      public bio:string,
+      public repos:number,
+      public followers:number,
+      public following:number,
+      public link: any,
+      public date: Date){}
+  }
+  
