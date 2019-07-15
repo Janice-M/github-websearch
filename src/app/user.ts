c54d07a8e865d02341e3cba868a2f31263cb1854
Janice-M <janiceink001@gmail.com>
2019-07-15 11:57:43 +0300

    shit is fluid

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,12 @@
 export class User {
     constructor(
-       avatar_url: any,
-       username:string,
-       bio:string,
-       repos:number,
-       followers:number,
-       following:number,
+      public avatar_url: any,
+      public username:string,
+      public bio:string,
+      public repos:number,
+      public followers:number,
+      public following:number,
+      public html_url : string,
       // public link: any,
       // public date: Date
       ){}
