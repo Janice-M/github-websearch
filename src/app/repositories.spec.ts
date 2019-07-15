2e6c2dab114c39477c788cccddd7bc0672ec4c0d
Janice-M <janiceink001@gmail.com>
2019-07-15 06:23:59 +0300

    component complete

---
+++ src/app/repositories.spec.ts
@@ -0,0 +1,7 @@
+import { Repositories } from './repositories';
+
+describe('Repositories', () => {
+  it('should create an instance', () => {
+    expect(new Repositories()).toBeTruthy();
+  });
+});
