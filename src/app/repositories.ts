fc0b0db7168fc1e8683047a7459a69455e74a790
Jan :) <janiceink001@gmail.com>
2021-02-11 12:49:29 +0300

    Update repositories.ts

--- src/app/repositories.ts
+++ src/app/repositories.ts
@@ -3,9 +3,7 @@ export class Repositories {
       public name:string,
       public description:string,
       public html_url : string
-      // public size:number,
-      // public url: any,
-      // public date: Date
+     
       )
       {}
   }
